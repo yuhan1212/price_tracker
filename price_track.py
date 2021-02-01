a8af2b6379b5ef84daa4ee2ad3f666f5a543c0ca
yuhan1212 <yuhan.chi12@gmail.com>
2021-01-31 20:56:20 -0800

    price_tracking get URL

---
+++ price_track.py
@@ -0,0 +1,8 @@
+import requests
+
+URL = "https://www.amazon.com/Stewarts-Cream-Soda-12oz-bottles/dp/B072FW8BDT/ref=sr_1_1?\
+    crid=3GE56HV1BXGUY&dchild=1&keywords=cream+soda+stewarts&qid=1612080523&sprefix=cream\
+        +soda+stew%2Caps%2C304&sr=8-1"
+
+headers = {"User-Agent":  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) \
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
